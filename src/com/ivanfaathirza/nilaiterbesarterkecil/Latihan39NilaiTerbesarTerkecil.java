786a60c14053d4322f1160dcf43c0ee03b0b5f27
Ivan Faathirza <49663880+ivan0811@users.noreply.github.com>
2020-11-07 16:51:47 +0700

    initial commit

--- src/com/ivanfaathirza/nilaiterbesarterkecil/Latihan39NilaiTerbesarTerkecil.java
+++ src/com/ivanfaathirza/nilaiterbesarterkecil/Latihan39NilaiTerbesarTerkecil.java
@@ -2,6 +2,17 @@ package com.ivanfaathirza.nilaiterbesarterkecil;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author Ivan
+ * NAMA         : Ivan Faathirza
+ * KELAS        : IF1
+ * NIM          : 10119003
+ * Deskripsi Program : program ini untuk menentukan nilai terkecil dan 
+ * terbesar dari nilai mahasiswa, berbasis objek dengan 
+ * instansiasi objek petugas
+ */
+
 public class Latihan39NilaiTerbesarTerkecil {
 
     public static void main(String[] args) {
